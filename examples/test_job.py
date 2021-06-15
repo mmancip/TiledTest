9990b9533c1dfe49cfa90334dad85da5472cbb96
mmancip <Martial.Mancip@MaisondelaSimulatipn.fr>
2021-06-15 15:31:48 +0200

    Use config.tar if exists.

--- examples/test_job.py
+++ examples/test_job.py
@@ -8,6 +8,9 @@ import argparse
 import re, datetime
 import json
 
+if (os.path.exists("config.tar")):
+    os.system("tar xf config.tar")
+
 SITE_config='./site_config.ini'
 CASE_config="./case_config.ini"
 
